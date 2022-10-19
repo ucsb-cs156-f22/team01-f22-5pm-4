c4b4d7b65d051c7938bdaedf75ae52d3b7a6fcae
briggsrr <robert.briggs.3@icloud.com>
2022-10-18 20:51:23 -0700

    bb - gradescope fix: readding bean with no cors configuration

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/config/SpringFoxConfig.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/config/SpringFoxConfig.java
@@ -51,25 +51,23 @@ public class SpringFoxConfig {
           .build();                                           
     }
 
-    // @Bean
-    // public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
-    //     List<ExposableEndpoint<?>> allEndpoints = new ArrayList<ExposableEndpoint<?>>();
-    //     Collection<ExposableWebEndpoint> endpoints = webEndpointsSupplier.getEndpoints();
-    //     allEndpoints.addAll(endpoints);
-    //     allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-    //     allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-    //     String basePath = webEndpointProperties.getBasePath();
-    //     EndpointMapping endpointMapping = new EndpointMapping(basePath);
-    //     EndpointLinksResolver linksResolver =  new EndpointLinksResolver(allEndpoints, basePath)
-    //     boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
-    //     CorsConfiguration corsConfiguration = corsProperties.toCorsConfiguration();
-    //     return new WebMvcEndpointHandlerMapping(endpointMapping, endpoints, endpointMediaTypes, corsConfiguration, linksResolver, shouldRegisterLinksMapping);
-    // }
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping);
+    }
 
 
-    // private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
-    //     return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
-    // }
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
 
 }
 
